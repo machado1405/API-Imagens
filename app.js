ec46054207ce51b3b2a7d137e840b305848c993a
Matheus Da Silva Machado <agromsmachado@gmail.com>
2023-03-17 19:44:53 -0300

    Primeiro commit api imagens

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log("Servidor rodando na porta " + port);
+});
